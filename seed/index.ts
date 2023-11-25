db7e2bc517c290e40b8dea504daef80de84d5934
ninest <parth.kabra@gmail.com>
2023-11-24 21:59:43 -0500

    Add categories

--- seed/index.ts
+++ seed/index.ts
@@ -2,6 +2,22 @@ import { prisma } from "@/db/prisma";
 
 (async () => {
   const moulton = await prisma.bot.create({
-    data: { slug: "seth-moulton", name: "Seth Moulton Bot", description: "The chatbot for Congressman Seth Moulton." },
+    data: {
+      slug: "seth-moulton",
+      name: "Seth Moulton Bot",
+      description: "The chatbot for Congressman Seth Moulton.",
+      categories: {
+        create: [
+          {
+            name: "Housing",
+            description: "Everything related to housing and homelessness.",
+          },
+          {
+            name: "Healthcare",
+            description: "Everything related to healthcare and health insurance",
+          },
+        ],
+      },
+    },
   });
 })();
